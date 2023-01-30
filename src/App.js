55adb5ff7c400b4f20a6b237250be3dfcf702c54
yitlelim <yit.le.a.lim@vialto.com>
2023-01-30 15:02:19 +1100

    with axios and SWR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import useSWR, { useSWRConfig } from "swr";
+
+const fetcher = url => axios.get(url).then(res => res.data)
 
 function App() {
+  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/users', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        <h1>Users List</h1> 
+        {data.map((data, idx) => (
+          <>
+            <p key={idx}>{data.name} - {data.website}</p>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
