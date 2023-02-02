40fd0266711cba73225fdafbc3fd2364bc2a3a68
yitlelim <yit.le.a.lim@vialto.com>
2023-02-02 17:58:51 +1100

    Added test app for SWR Post and the Hook

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import SwrPost from './swrPost';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <SwrPost /> */}
   </React.StrictMode>
 );
