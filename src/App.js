286ef77b53e6a98ff028a5a3e4d9dfc31cd72dc6
yitlelim <yit.le.a.lim@vialto.com>
2023-02-13 19:15:36 +1100

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-// import './App.css';
-// import axios from 'axios';
-import useSWR, { mutate } from "swr";
-// import { Button } from 'react-bootstrap';
+// import useSWR, { mutate } from "swr";
 import React, { useState } from 'react';
 import { useCrud } from './hooks/use-crud.tsx';
 
@@ -11,14 +8,14 @@ function App() {
   const [formData, setFormData] = useState({ name: '', age: '' });
   const { create } = useCrud(url, url);
   const { fetching } = useCrud(url);
-  // console.log(fetch)
 
-  let listOfData = [];
+  //GET and display
+  var listOfData = [];
   if(fetching && fetching.data) {
-    let actualData = fetching.data.payload || {};
+    var actualData = fetching.data.payload || {};
     for (const key in actualData) {
       if (actualData.hasOwnProperty(key)) {
-        listOfData.push(key + ': ' + actualData[key].name);
+        listOfData.push(actualData[key]);
       }
     }
   }
@@ -33,15 +30,10 @@ function App() {
   //   mutate(await response.json(), false);
   // };
 
+  // Create
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Submit 1 console");
-    // console.log(formData);
-    const response = await create(formData, true);
-    // setTimeout(() => {
-    //   console.log("Submit 2 console");
-    //   // console.log(response);
-    // }, 10);
+    await create(formData, true);
   }
   
   const handleChange = (event) => {
@@ -51,9 +43,9 @@ function App() {
     });
   };
   
-  const { data, mutate } = useSWR(url, () =>
-    fetch(url).then((res) => res.json())
-  );
+  // const { data, mutate } = useSWR(url, () =>
+  //   fetch(url).then((res) => res.json())
+  // );
   
   return (
     <form onSubmit={handleSubmit}>
@@ -69,50 +61,15 @@ function App() {
       <br />
       <button type="submit">Submit</button>
       <br />
-      {listOfData}
+      <ul>
+        {listOfData.map((item, index) => (
+          <li key={index}>
+            {index}. {item.name}, aged {item.age}.
+          </li>
+        ))}
+      </ul>
     </form>
   )
 }
-
-
-// // const fetcher = url => axios.get(url).then(res => res.data)
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
-// // let user = {"id":11,"name": "James", "age": 23};
-// let posts = {"userId":11,"id":11,"title": "James", "body": "Happy happy day"};
-
-// function App() {
-//   const { data, error } = useSWR('http://jsonplaceholder.typicode.com/users', fetcher, {
-//     revalidateOnFocus: false,
-//   })
-
-//   if (error) return <div>Failed to load</div>
-//   if (!data) return <div>Loading...</div>
-
-//   return (
-//     <div className="App">
-//       <div className='App-header'>
-//         <h1>Users List</h1> 
-//         {data.map((data, idx) => (
-//           <>
-//             <p key={idx}>{data.name}</p>
-//           </>
-//         ))}
-//       </div>
-//       <Button 
-//         onClick={async () => {
-//           await fetcher('http://localhost:3000/data', {
-//           // await fetcher('http://jsonplaceholder.typicode.com/posts', {
-//             method: 'POST',
-//             body: JSON.stringify(posts),
-//           });
-//           mutate('http://localhost:3000/data');
-//           // mutate('http://jsonplaceholder.typicode.com/posts');
-//         }}
-//       >
-//         Add Posts
-//       </Button>
-//     </div>
-//   );
-// }
 
 export default App;
